07d0383df38ad714cad3c195b5aa3ae447c168a6
Atolycs <111569596+atolycs@users.noreply.github.com>
2024-06-06 13:40:47 +0900

    ci: build test Ci

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo "Building Windows execution..."
 make win64
+echo "Building Linux execution..."
 make linux
-make GOARCH=arm GOARM=6 linux
+
+echo "Building Linux ARM execution..."
+make CC=arm-linux-gnueabihf-gcc GOARCH=arm GOARM=6 linux
