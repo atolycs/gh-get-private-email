b3a610c9d6e6c78ee6fd3619b52f70c82a061cb8
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-01-16 14:16:52 +0900

    test: code

--- build.sh
+++ build.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-$GOBUILD = "go build"
+$GOBUILD="go build"
 
-$VERSION = $(git tag -l --sort=v:refname "v*" | tail -1)
-$APP_NAME = gh-get-private-email
-$BASE_FILENAME = "${APP_NAME}_${VERSION}"
+$VERSION=$(git tag -l --sort=v:refname "v*" | tail -1)
+$APP_NAME="gh-get-private-email"
+$BASE_FILENAME="${APP_NAME}_${VERSION}"
 
 $DEST_FOLDER = "dist/"
 
 # Output artifacts
-$WINBIN = "${DEST_FOLDER}/${BASE_FILENAME}-windows-$(go env GOARCH).exe"
-$WINGOOPTS = "-x -ldflags \"-static -X github.com/atolycs/gh-get-private-email/internal/version.version=${VERSION}\""
+$WINBIN="${DEST_FOLDER}/${BASE_FILENAME}-windows-$(go env GOARCH).exe"
+$WINGOOPTS="-x -ldflags \"-static -X github.com/atolycs/gh-get-private-email/internal/version.version=${VERSION}\""
 if [ "$COMSPEC" != "" ]; then
   echo "Windows Build mode"
-  CGO_ENABLED=1 $(GOBUILD) -o $WINBIN $WINGOOPTS
+  CGO_ENABLED=1 $(GOBUILD) $WINGOOPTS -o $WINBIN main.go
 else
   make linux
   make CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ GOARCH=arm GOARM=6 linux
