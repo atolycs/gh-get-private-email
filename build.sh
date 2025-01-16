14d69472a36a26e37570e807bf27bfc6dfb4af7b
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-01-16 14:47:07 +0900

    test: windows code test

--- build.sh
+++ build.sh
@@ -10,11 +10,12 @@ DEST_FOLDER="dist/"
 
 # Output artifacts
 WINBIN="${DEST_FOLDER}/${BASE_FILENAME}-windows-$(go env GOARCH).exe"
-WINGOOPTS="-x -ldflags="-extldflags='-static' -X github.com/atolycs/gh-get-private-email/internal/version.version=${VERSION}""
+WINGOOPTS="-x -ldflags="-extldflags='-static'""
+VERSION_OPTS="-ldflags="-X github.com/atolycs/gh-get-private-email/internal/version.version=${VERSION}""
 
 if [ "$COMSPEC" != "" ]; then
   echo "Windows Build mode"
-  CGO_ENABLED=1 ${GOBUILD} $WINGOOPTS -o $WINBIN
+  CGO_ENABLED=1 ${GOBUILD} $WINGOOPTS $VERSION_OPTS -o $WINBIN
 else
   make linux
   make CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ GOARCH=arm GOARM=6 linux
