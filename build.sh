111d02cca6b1965d49faccef3045a45bbe222bcf
Atolycs <111569596+atolycs@users.noreply.github.com>
2025-08-19 23:16:06 +0900

    fix: build script

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ if [ "$COMSPEC" != "" ]; then
   $WINBIN
 else
   make linux
-  if [ $CI ] && [ $RUUNER_OS == 'ubuntu-24.04-arm' ]; then
+  if [ $CI ] && [ "${RUUNER_OS}" == "ubuntu-24.04-arm" ]; then
     make CC=arm-linux-gnueabihf-gcc CXX=arm-linux-gnueabihf-g++ GOARCH=arm GOARM=6 linux
   fi
 fi
